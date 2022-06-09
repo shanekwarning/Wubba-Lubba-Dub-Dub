b965afe1ef1ae9258b16ddfbad1cafba924d79b0
Shane Warning <shane.k.warning@gmail.com>
2022-06-09 14:59:54 -0500

    Add styling to components and episodes

--- src/CharacterContainer/CharacterContainer.js
+++ src/CharacterContainer/CharacterContainer.js
@@ -8,20 +8,24 @@ const CharacterContainer = (characters) => {
     console.log('is this what you are looking for', characters.characters.results[0])
 
     const characterCards = characters.characters.results.map(character => {
-        return <NavLink to={`/character/${character.id}`} key={character.id}>
+        return <div key={character.id}>
+            <NavLink to={`/character/${character.id}`}>
 
-            <CharacterCards
-                image={character.image}
-                name={character.name}
-                id={character.id}
-            />
-        </NavLink>
+                <CharacterCards
+                    image={character.image}
+                    name={character.name}
+                    id={character.id}
+                />
+            </NavLink>
+            <p className='character-name'>{character.name}</p>
+        </div>
 
     })
 
     return (
         <section className='card-container'>
             {characterCards}
+
         </section>
     )
 }
--- src/CharacterPage/CharacterPage.js
+++ src/CharacterPage/CharacterPage.js
@@ -1,26 +1,66 @@
 import React, { Component } from 'react'
+import './CharacterPage.css'
 
 class CharacterPage extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            character: []
+            character: ''
         }
     }
 
     componentDidMount() {
-        // this.setState({ character: this.props.characterPage })
         fetch(`https://rickandmortyapi.com/api/character/${this.props.characterPage}`)
             .then(data => data.json())
             .then(data => this.setState({ character: data }))
     }
 
+
+
+
+
     render() {
+        let charactersEpisodes;
+        let show;
+
+        // if (this.state.character === '') {
+        //     return charactersEpisodes = ''
+        // } else {
+        //     charactersEpisodes = this.state.character.episodes.map(episode => {
+        //         console.log(episode.split('/'))
+        //         return <p> {episode.split('/').slice(-1)} </p>
+        //     })
+        // }
+        if (this.state.character === '') {
+            return
+        } else if (this.state.character !== '') {
+            console.log(this.state.character.episode[0])
+            charactersEpisodes = this.state.character.episode.map(episode => {
+                show = episode.toString().split('/').splice(-1)
+                return <p className="episodes">Episode{show}</p>
+            })
+
+        }
+        console.log(show)
+        console.log(charactersEpisodes)
         return (
-            <section>
-                <div>
-                    {this.state.character ? <img src={this.state.character.image} /> : ''}
-                </div>
+            <section className='character-page-main'>
+                {this.state.character ?
+                    <div className='character-page-styling'>
+                        <div className='character-photo-container'>
+                            <img className='character-photo' src={this.state.character.image} />
+                        </div>
+                        <div>
+                            <p>Name: {this.state.character.name}</p>
+                            <p>Gender: {this.state.character.gender}</p>
+                            <p>Species: {this.state.character.species}</p>
+                            <p>Status: {this.state.character.status}</p>
+                            {this.state.character.type ? <p>Type of Being :{this.state.character.type}</p> : ''}
+                            {origin ? <p>Origin: {this.state.character.origin.name}</p> : ''}
+                        </div>
+                    </div> : ''}
+
+                {charactersEpisodes ? <div className='episode-container'>{charactersEpisodes} </div> : ''}
             </section>
         )
     }
